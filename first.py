678f71d206f4b70790af20356c114633ffbbe581
A-atmos <aavashchhetri01@gmail.com>
2021-03-29 21:43:22 +0545

    popGUI

--- first.py
+++ first.py
@@ -1,1 +1,14 @@
-import PyQt5
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+def window():
+    app = QApplication(sys.argv) #os application window
+    win = QMainWindow()
+    win.setGeometry(100, 70, 500 ,500)
+    win.setWindowTitle('Just a try!')
+
+    win.show()
+    sys.exit(app.exec_())
+
+window()
