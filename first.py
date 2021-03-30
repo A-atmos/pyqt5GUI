98ee7b36954dfc6624992954ae1d5bced7ccb09b
A-atmos <aavashchhetri01@gmail.com>
2021-03-30 11:26:18 +0545

    Button

--- first.py
+++ first.py
@@ -2,6 +2,9 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+def clicked():  #function after clicking the b1
+    print('Clicked')
+
 def window():
     app = QApplication(sys.argv) #os application window
     win = QMainWindow()
@@ -11,6 +14,12 @@ def window():
     label = QtWidgets.QLabel(win)
     label.setText('Label')
     label.move(50,50)
+
+    b1 = QtWidgets.QPushButton(win)
+    b1.setText('Button1')
+    b1.move(65,65)
+    b1.clicked.connect(clicked) #clicked to call the above function
+
     win.show()
     sys.exit(app.exec_())
 
